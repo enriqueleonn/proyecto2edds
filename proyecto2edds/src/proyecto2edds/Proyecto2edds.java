09e70548d84905b25e2ba6f5ace6239c32b3934d
enriqueleonn <ealu2005@gmail.com>
2024-11-12 23:24:27 -0400

    Conf. Inicial

---
+++ proyecto2edds/src/proyecto2edds/Proyecto2edds.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2edds;
+
+/**
+ *
+ * @author Enrique León
+ */
+public class Proyecto2edds {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
